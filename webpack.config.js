4b382d683ea8c2e7e9b45f18538b47cdb572be68
tunglx2 <tunglx2@vng.com.vn>
2023-03-06 10:24:07 +0700

    init engine

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: { titan: './src/index.ts' },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].bundle.js',
+        clean: true,
+        library: {
+            name: 'Titan',
+            type: 'var',
+        },
+    },
+    mode: process.env.NODE_ENV || 'development',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    devServer: {
+        historyApiFallback: {
+            index: '/index.html',
+        },
+        port: 3001,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            compilerOptions: { noEmit: false },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[local]-[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                exclude: /\.module\.s(a|c)ss$/,
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(), new TerserWebpackPlugin()],
+        removeAvailableModules: true,
+        removeEmptyChunks: true,
+        mergeDuplicateChunks: true,
+        emitOnErrors: false,
+        concatenateModules: true,
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html'),
+            favicon: path.join(__dirname, 'public', 'favicon.ico'),
+            filename: 'index.html',
+            chunks: ['titan'],
+            inject: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+        }),
+        new CircularDependencyPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{ from: 'public/res', to: 'res' }],
+        }),
+    ],
+};
